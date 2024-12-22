fa7dd5ea5cab238852c19aead132092408d0fb21
David Zwicker <david.zwicker@ds.mpg.de>
2024-12-22 15:18:34 +0100

    Add example for density plot

---
+++ examples/densityplot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from tools import densityplot
+
+x = np.geomspace(1, 1e2, 6)
+y = np.linspace(0, 9, 4)
+data = np.random.uniform(size=(len(x), len(y)))
+
+densityplot(data, x, y, vmin=0, vmax=1)
+plt.colorbar()
+plt.show()
