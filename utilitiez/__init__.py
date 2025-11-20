a1d68cfe2c66c0aff98c874556d955540b891f9a
David Zwicker <david.zwicker@ds.mpg.de>
2025-11-20 11:35:16 +0100

    Added version information to package

--- utilitiez/__init__.py
+++ utilitiez/__init__.py
@@ -1,7 +1,23 @@
-"""Various useful tools that we developed.
+"""Various useful tools that we developed in the Zwicker Group.
 
 .. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
 """
 
+# determine the package version
+try:
+    # try reading version of the automatically generated module
+    from ._version import __version__
+except ImportError:
+    # determine version automatically from CVS information
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        __version__ = version("utilitiez")
+    except PackageNotFoundError:
+        # package is not installed, so we cannot determine any version
+        __version__ = "unknown"
+    del PackageNotFoundError, version  # clean name space
+
+# import the functions provided by the package
 from .densityplot import densityplot
 from .mathematics import *
